3b402270f81284bf66e0ffc88b694320af4b7df9
Purple-Chicken <purple.chickn@rutgers.edu>
2026-07-16 17:45:58 -0400

    install comment -PC

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash 
-
+# curl https://raw.githubusercontent.com/Purple-Chicken/bash-workshop/refs/heads/main/setup.sh | bash
 git clone https://github.com/Purple-Chicken/bash-workshop.git 
 cd bash-workshop/ctf
 chmod u+x ../tutor.sh 
